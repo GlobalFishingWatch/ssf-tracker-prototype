97f4b01d95379ddcfc121437729becf067db66d2
Quinn Yates <quinnyates3229@gmail.com>
2024-02-06 16:59:19 -0500

    Fixes to gps.py:
    Added `check_gps` event, created recurring one second timer to execute `check_gps`, and parse/store location in `on_check_gps`

--- controller/app/gps.py
+++ controller/app/gps.py
@@ -5,6 +5,67 @@ from statemachine import Transition
 from statemachine import StateMachine
 from timer import Timer
 from machine import UART
+import nmea
+
+
+class MPYGPS(StateMachine):
+    states = [
+        State('sleep', on_enter='on_enter_sleep'),
+        State('locating', on_enter='on_enter_locating'),
+        State('ready', on_enter='on_enter_ready'),
+    ]
+    transitions = [
+        Transition('locate', 'sleep', 'locating'),
+        Transition('check_gps', after='on_check_gps'),
+        Transition('gps_ready', 'locating', 'ready'),
+        Transition('sleep', None, 'sleep'),
+
+    ]
+
+    def __init__(self, gps_timeout_ms=2000, on_ready_event=None, timer=None, baudrate=9600, tx=8, rx=7, **kwargs):
+        super(MPYGPS, self).__init__(states=self.states, transitions=self.transitions, **kwargs)
+        self.gps_timeout_ms = gps_timeout_ms
+        self.on_ready_event = on_ready_event
+        self.gps_timer = timer if timer else Timer(event=self.get_event('check_gps'), duration_ms=1000, recurring=True)
+        self._last_location = None
+        self.tx = tx
+        self.rx = rx
+
+        self.uart = UART(1, baudrate)
+        self.uart.init(baudrate, tx=self.tx, rx=self.rx)
+
+    @property
+    def last_location(self):
+        # get the most recent recorded location.  Could be None
+        return self._last_location
+
+    def on_enter_locating(self, event):
+        # start a timer that will transition us to ready
+        # if(gps
+        self.gps_timer.reset()
+        print("Enter Locating")
+
+    def on_check_gps(self, event):
+        # After event check_gps which is executed by timer
+        # Check for fix: If fix, update location and trigger gps_ready
+        msg_str = self.uart.readline().decode().strip()
+        msg = nmea.parse_sentence(message_str)
+
+        if msg['fix_quality'] == 1:
+            self._last_location = msg
+            self.get_event('gps_ready').trigger()
+
+    def on_enter_ready(self, event):
+        # If we have a fix, log message
+        self.gps_timer.cancel()
+        print("GPS READY")
+        self.on_ready_event.trigger()
+
+    def on_enter_sleep(self, event):
+        # cancel any running timer and clear the last location
+        # pretend to sleep
+        self.gps_timer.cancel()
+        self._last_location = None
 
 
 class MockGPS(StateMachine):
@@ -17,6 +78,7 @@ class MockGPS(StateMachine):
         Transition('locate', 'sleep', 'locating'),
         Transition('gps_ready', 'locating', 'ready'),
         Transition('sleep', None, 'sleep'),
+
     ]
 
     def __init__(self, gps_timeout_ms=2000, on_ready_event=None, timer=None, **kwargs):
@@ -39,45 +101,6 @@ class MockGPS(StateMachine):
         # pretend that we have fixed a GPS position
         self.gps_timer.cancel()
         self._last_location = dict(lat=22, lon=33, timestamp=Timer.current_time_ms())
-        self.on_ready_event.trigger()
-
-    def on_enter_sleep(self, event):
-        # cancel any running timer and clear the last location
-        # pretend to sleep
-        self.gps_timer.cancel()
-        self._last_location = None
-
-
-class MPYGPS(MockGPS):
-    def __init__(self, gps_timeout_ms=2000, on_ready_event=None, timer=None, baudrate=9600, tx=8, rx=7, **kwargs):
-        super(MPYGPS, self).__init__(**kwargs)
-        self.gps_timeout_ms = gps_timeout_ms
-        self.on_ready_event = on_ready_event
-        self.gps_timer = timer if timer else Timer(event=self.get_event('gps_ready'), duration_ms=self.gps_timeout_ms)
-        self._last_location = None
-        self.tx = tx
-        self.rx = rx
-
-        self.uart = UART(1, baudrate)
-        self.uart.init(baudrate, tx=self.tx, rx=self.rx)
-
-    @property
-    def last_location(self):
-        # get the most recent recorded location.  Could be None
-        return self._last_location
-
-    def on_enter_locating(self, event):
-        # start a timer that will transition us to ready
-        # if(gps
-        self.gps_timer.reset()
-        print("Enter Locating")
-
-    def on_enter_ready(self, event):
-        # If we have a fix, log message
-        self.gps_timer.cancel()
-        self._last_location = self.uart.readline()
-        # log_location(self._last_location)
-        print("GPS READY")
         self.on_ready_event.trigger()
 
     def on_enter_sleep(self, event):
---
+++ controller/app/test_nmea.py
@@ -0,0 +1,168 @@
+import unittest
+
+import nmea
+from nmea import split_sentence
+from nmea import split_body
+from nmea import compute_checksum
+from nmea import create_sentence
+from nmea import parse_int
+from nmea import parse_float
+from nmea import parse_degrees
+from nmea import parse_date
+from nmea import parse_lat_lon
+from nmea import parse_gpgga_args
+from nmea import parse_gprmc_args
+from nmea import parse_sentence
+
+
+class TestNMEA(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_split_sentence(self):
+        params = [
+            ("", ("", "")),
+            ("$", ("", "")),
+            ("$*", ("", "")),
+            ("$CMD", ("CMD", "")),
+            ("$CMD,1,2,3*FF", ("CMD,1,2,3", "FF")),
+        ]
+        for sentence, expected in params:
+            with self.subTest(sentence=sentence, expected=expected):
+                self.assertEqual(split_sentence(sentence), expected)
+
+    def test_split_body(self):
+        params = [
+            ("", ("", [])),
+            ("CMD", ("CMD", [])),
+            ("CMD,1,2,3", ("CMD", ["1", "2", "3"])),
+        ]
+        for body, expected in params:
+            with self.subTest(body=body, expected=expected):
+                self.assertEqual(split_body(body), expected)
+
+    def test_compute_checksum(self):
+        params = [
+            ("", "00"),
+            ("CMD", "4A"),
+        ]
+        for body, expected in params:
+            with self.subTest(body=body, expected=expected):
+                self.assertEqual(compute_checksum(body), expected)
+
+    def test_create_sentence(self):
+        params = [
+            ("", "$*00"),
+            ("CMD", "$CMD*4A"),
+        ]
+        for body, expected in params:
+            with self.subTest(body=body, expected=expected):
+                self.assertEqual(create_sentence(body), expected)
+
+    def test_parse_int(self):
+        params = [
+            ("", None),
+            ("A", None),
+            ("2", 2),
+        ]
+        for value, expected in params:
+            with self.subTest(value=value, expected=expected):
+                self.assertEqual(parse_int(value), expected)
+
+    def test_parse_float(self):
+        params = [
+            ("", None),
+            ("A", None),
+            ("2", 2.0),
+        ]
+        for value, expected in params:
+            with self.subTest(value=value, expected=expected):
+                self.assertEqual(parse_float(value), expected)
+
+    def test_parse_degrees(self):
+        params = [
+            ("", None),
+            ("A", None),
+            ("1230.00", 12.5),
+        ]
+        for value, expected in params:
+            with self.subTest(value=value, expected=expected):
+                self.assertEqual(parse_degrees(value), expected)
+
+    def test_parse_date(self):
+        params = [
+            ("", (None, None, None)),
+            ("0112", (None, 12, 1)),
+            ("221133", (2033, 11, 22)),
+        ]
+        for value, expected in params:
+            with self.subTest(value=value, expected=expected):
+                self.assertEqual(parse_date(value), expected)
+
+    def test_parse_lat_lon(self):
+        params = [
+            ("", "", None),
+            ("A", "A",  None),
+            ("1230.00", "E", 12.5),
+            ("1230.00", "W", -12.5),
+        ]
+        for degree, hemisphere, expected in params:
+            with self.subTest(degree=degree, hemisphere=hemisphere, expected=expected):
+                self.assertEqual(parse_lat_lon(degree, hemisphere), expected)
+
+    def test_parse_gpgga_args(self):
+        params = [
+            ("235317.000,4003.9039,N,10512.5793,W,1,08,1.6,1577.9,M,-20.7,M,,0000",
+             {'height_geoid': -20.7, 'time_utc': (23, 53, 17), 'horizontal_dilution': 1.6, 'longitude': -105.209655,
+              'satellites': 8, 'fix_quality': 1, 'altitude_m': 1577.9, 'latitude': 40.065065}),
+        ]
+        for args, expected in params:
+            with self.subTest(args=args, expected=expected):
+                args = dict(zip(nmea.SENTENCE_TYPES['GPGGA'], args.split(',')))
+                actual = parse_gpgga_args(args)
+                for k in expected.keys():
+                    self.assertAlmostEqual(actual[k], expected[k], places=4)
+
+    def test_parse_gprmc_args(self):
+        params = [
+            ("235316.000,A,4003.9040,N,10512.5792,W,0.09,144.75,141112,,",
+             {'utc_seconds': 1352955196, 'status': 'A', 'latitude': 40.065067,
+              'speed_knots': 0.09, 'date': (2012, 11, 14)}
+             ),
+        ]
+        for args, expected in params:
+            with self.subTest(args=args, expected=expected):
+                args = dict(zip(nmea.SENTENCE_TYPES['GPRMC'], args.split(',')))
+                actual = parse_gprmc_args(args)
+                for k in expected.keys():
+                    self.assertEqual(actual[k], expected[k])
+
+    def test_parse_sentence(self):
+        params = [
+            ("$GPGGA,BAD_DATA,0000*5F",
+             {'error': 'Invalid checksum'}),
+            ("$GPRMC,235316.000,A,4003.9040,N,10512.5792,W,0.09,144.75,141112,,*19",
+             {'time_utc': (23, 53, 16), 'status': 'A', 'latitude': 40.065067,
+              'speed_knots': 0.09, 'date': (2012, 11, 14)}),
+            ("$GPGGA,235317.000,4003.9039,N,10512.5793,W,1,08,1.6,1577.9,M,-20.7,M,,0000*5E",
+             {'time_utc': (23, 53, 17), 'sentence_type': 'GPGGA'}),
+            ("$GPGSA,A,3,22,18,21,06,03,09,24,15,,,,,2.5,1.6,1.9*3E",
+             {'sentence_type': 'GPGSA', 'field1': 'A'}),
+            ("$GPRMC,000629.000,A,3927.8092,N,07747.8769,W,0.53,347.18,220923,,,A*79",
+             {'status': 'A', 'fix_quality': 1}),
+
+        ]
+        for sentence, expected in params:
+            with self.subTest(sentence=sentence, expected=expected):
+                actual = parse_sentence(sentence)
+                self.assertEqual(actual['sentence'], sentence)
+                for k in expected.keys():
+                    self.assertEqual(actual[k], expected[k])
+
+
+if __name__ == '__main__':
+    unittest.main()
